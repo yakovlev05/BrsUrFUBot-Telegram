499745649a1d8f759284aa19f25a3c4afbab9403
yakovlev05 <alexey.yakovlev.05@mail.ru>
2024-06-17 19:45:55 +0500

    update db in UserService

--- UserService/Data/Entities/UserEntity.cs
+++ UserService/Data/Entities/UserEntity.cs
@@ -5,6 +5,7 @@ public class UserEntity
     public int Id { get; init; }
     public string? UrfuLogin { get; set; }
     public string? UrfuPassword { get; set; }
+    public bool IsAuthorizedInUrfu { get; set; }
     public DateTime RegisteredInBot = DateTime.Now.ToUniversalTime();
     public int TelegramId { get; set; }
     public string TelegramUsername { get; set; } = null!;
---
+++ UserService/Data/Migrations/20240617144534_add IsAuthorizedInUrfu.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIsAuthorizedInUrfu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAuthorizedInUrfu",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAuthorizedInUrfu",
+                table: "Users");
+        }
+    }
+}
